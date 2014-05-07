0783f832bfe85d91bfb01b070602d7e8b7da7b64
socalblue <joseph3porto@gmail.com>
2014-05-07 13:04:31 -0700

    Add validation to ensure names are specified when creating projects

--- spec/features/creating_projects_spec.rb
+++ spec/features/creating_projects_spec.rb
@@ -1,8 +1,10 @@
 require 'spec_helper'
 feature 'Creating Projects' do
-  scenario "can create a project" do
+  before do
     visit '/'
     click_link 'New Project'
+  end
+  scenario "can create a project" do
     fill_in 'Name', with: 'TextMate 2'
     fill_in 'Description', with: 'A text-editor for OS X'
     click_button 'Create Project'
@@ -11,7 +13,12 @@ feature 'Creating Projects' do
     expect(page.current_url).to eql(project_url(project))
     title = "TextMate 2 - Projects - Ticketee"
     expect(page).to have_title(title)
-
+  end
+  scenario "can not create a project without a name" do
+    click_button 'Create Project'
+    expect(page).to have_content("Project has not been created.")
+    expect(page).to have_content("Name can't be blank")
   end
 end
+
 
